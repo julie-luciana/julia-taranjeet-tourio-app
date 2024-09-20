6e454c224f4e9481a029059fc8a57b8f94553955
Taranjeet Singh <taranjeetsinghtanu2@gmail.com>
2024-09-20 16:43:49 +0200

    Completed with read Data

--- db/models/Place.js
+++ db/models/Place.js
@@ -1,27 +1,4 @@
-/*import mongoose from "mongoose";
-
-const PlaceSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-});
-
-export default mongoose.models.Places || mongoose.model ('Place', PlaceSchema);
-*/ 
-// model to structure data & CRUD
-/**
- *import mongoose from "mongoose";
-
-const { Schema } = mongoose;
-const placeSchema = new Schema({
-  name: String,
-  description: String,
-});
-
-const Place = mongoose.models.Place || mongoose.model("Place", placeSchema);
-export default Place;
- */
-
- import mongoose from "mongoose";
+import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 const placeSchema = new Schema({
@@ -30,7 +7,7 @@ const placeSchema = new Schema({
   image: { type: String, required: true },
   mapURL: { type: String, required: true },
   description: { type: String, required: true },
-  comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }], 
+  
 });
 
 const Place = mongoose.models.Place || mongoose.model("Place", placeSchema);
--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,3 +1,4 @@
+
 //import { db_places } from "../../../../lib/db_places";
 //import { db_comments } from "../../../../lib/db_comments";
 import dbConnect from "../../../../db/connect";
@@ -5,38 +6,19 @@ import Place from "../../../../db/models/Place";
 
 
 export default async function handler(request, response) {
-  
+
   await dbConnect();
-   const { id } = request.query;
-if (request.method === 'GET') {
+  const { id } = request.query;
+   
+    if (request.method === "GET") {
+  
   try {
     const place = await Place.findById(id);
-    if(!place) return res.status(404).jason({message:'Place not found'});
-    res.status(200).json(place);
+    if (!place) return response.status(404).json({ status: "Not found" });
+    response.staus(200).json(place);
   } catch (error) {
-    response.status(500).json({erorr: 'Not Found'});
+    response.status(500).json({error: 'Not Found'});
   }
 }
-
-/*
-   const place = await Place.findById(id);
-
-  if (!id) {
-    return;
   }
-
-  const place = db_places.find((place) => place._id.$oid === id);
-  const comment = place?.comments;
-  const allCommentIds = comment?.map((comment) => comment.$oid) || [];
-  const comments = db_comments.filter((comment) =>
-    allCommentIds.includes(comment._id.$oid)
-  );
-
-  if (!place) {
-    return response.status(404).json({ status: "Not found" });
-  }
-
-  response.status(200).json({ place: place, comments: comments });
-  */
-}
 
